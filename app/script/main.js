2a2912fdb044f8eb13f0f4e0ce13815568c215c0
Mike Chambers <83576928+mainfunktion@users.noreply.github.com>
2022-02-22 04:09:21 -0800

    validated input field

--- app/script/main.js
+++ app/script/main.js
@@ -1,3 +1,16 @@
+let validate = () => {
+    let UserInput = document.getElementById("search");
+    const btn = document.querySelector("button");
+
+    if(UserInput.value.trim() ==="") {
+        UserInput.placeholder = "Please enter a valid IP address or Domain";
+        return false
+    }   else {
+     alert("Loading");
+     true
+    }
+}
+
 var map = L.map('map').setView([51.505, -0.09], 13);
 
 var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
